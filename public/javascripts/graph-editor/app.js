46b824417cbffeb820bafb9944b0c3ed2f5a4a50
Rémi Bèges <remi.beges@gmail.com>
2015-11-17 14:51:06 +0100

    Customizing app.js file for reusability

--- public/javascripts/graph-editor/app.js
+++ public/javascripts/graph-editor/app.js
@@ -7,15 +7,17 @@ var vis = d3.select("body").append("svg:svg")
     .attr("height", h);
 
 d3.json("javascripts/graph-editor/graph.json", function(json) {
-    var force = self.force = d3.layout.force()
+    /*var force = self.force = d3.layout.force()
         .nodes(json.nodes)
         .links(json.links)
         .gravity(.05)
         .distance(100)
         .charge(-100)
         .size([w, h])
-        .start();
+        .start();*/
 
+
+/*
     var link = vis.selectAll("line.link")
         .data(json.links)
         .enter().append("svg:line")
@@ -24,36 +26,22 @@ d3.json("javascripts/graph-editor/graph.json", function(json) {
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
-
+*/
     var node_drag = d3.behavior.drag()
         .on("dragstart", dragstart)
         .on("drag", dragmove)
         .on("dragend", dragend);
 
-    function dragstart(d, i) {
-        force.stop() // stops the force auto positioning before you start dragging
-    }
-
-    function dragmove(d, i) {
-        d.px += d3.event.dx;
-        d.py += d3.event.dy;
-        d.x += d3.event.dx;
-        d.y += d3.event.dy;
-        tick(); // this is the key to make it work together with updating both px,py,x,y on d !
-    }
-
-    function dragend(d, i) {
-        d.fixed = true; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
-        tick();
-        force.resume();
-    }
-
-
     var node = vis.selectAll("g.node")
         .data(json.nodes)
       .enter().append("svg:g")
+        .attr("nodeid",function(d,i){ return i})
         .attr("class", "node")
+        .attr("x",function(d,i) { return 100 * i; })
+        .attr("y",function(d,i) { return 10 * i; })
+        .attr("transform", function(d,i) { return "translate(" + 100 * i + "," + 50 + ")"; })
         .call(node_drag);
+      console.log(node);
 
     node.append("svg:image")
         .attr("class", "circle")
@@ -69,8 +57,27 @@ d3.json("javascripts/graph-editor/graph.json", function(json) {
         .attr("dy", ".35em")
         .text(function(d) { return d.name });
 
-    force.on("tick", tick);
+    function dragstart(d, i) {
+        //force.stop() // stops the force auto positioning before you start dragging
+    }
 
+    function dragmove(d, i) {
+        var n = d3.select(this);
+        console.log(d)
+        n.attr("x",function(){return n.x + d3.event.dx})
+        n.attr("transform", function() { return "translate(" + 100 + "," + 200 + ")"; });
+        //tick(); // this is the key to make it work together with updating both px,py,x,y on d !
+    }
+
+    function dragend(d, i) {
+        d.fixed = true; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
+        //tick();
+        //force.resume();
+    }
+
+
+    //force.on("tick", tick);
+/*
     function tick() {
       link.attr("x1", function(d) { return d.source.x; })
           .attr("y1", function(d) { return d.source.y; })
@@ -79,6 +86,6 @@ d3.json("javascripts/graph-editor/graph.json", function(json) {
 
       node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
     };
-
+*/
 
 });
