757741445e45e0c594835ec1414702e29ec21c70
Rémi Bèges <remi.beges@gmail.com>
2015-11-18 17:40:51 +0100

    Two nodes can now be connected :fireworks:

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,4 +5,17 @@ jsPlumb.bind("ready", function() {
 
   jsPlumb.draggable("item_left");
   jsPlumb.draggable("item_right");
+
+  jsPlumb.addEndpoint("item_left", {
+  endpoint:"Dot",
+  isSource:true,
+  isTarget:true,
+  anchor:[ "Perimeter", { shape:"Square" } ]
+  });
+  jsPlumb.addEndpoint("item_right", {
+  endpoint:"Dot",
+  isSource:true,
+  isTarget:true,
+  anchor:[ "Perimeter", { shape:"Square" } ]
+  });
 });
