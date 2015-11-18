4b15092178ceb028abb46366637cd715512ec053
Rémi Bèges <remi.beges@gmail.com>
2015-11-18 17:31:36 +0100

    Basic draggable element

---
+++ public/javascripts/app.js
@@ -0,0 +1,8 @@
+jsPlumb.bind("ready", function() {
+
+  // your jsPlumb related init code goes here
+  jsPlumb.setContainer("conteneur");
+
+  jsPlumb.draggable("item_left");
+  jsPlumb.draggable("item_right");
+});
