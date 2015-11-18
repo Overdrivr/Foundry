7ebf11e528b344ccc609bb032074fc5f0f95fdc0
Rémi Bèges <remi.beges@gmail.com>
2015-11-18 23:00:22 +0100

    A node can now be created from a button click

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -13,10 +13,35 @@ jsPlumb.bind("ready", function() {
   anchor:[ 1, 0.8, 1, 0 ]
   });
 
-  jsPlumb.addEndpoint("item_right", {
+
+});
+
+var nodeAmount = 0;
+
+function createNode(){
+  jsPlumb.setContainer("conteneur");
+  var data = {
+    inputs:5,
+    outputs:3
+  }
+
+  var body = d3.select("div.cont");
+  console.log(body)
+  var div = body.append("div")
+              .attr("class","item")
+              .attr("id","toto");
+
+      div.append("p")
+        .text("Hey")
+
+  console.log(div);
+
+  jsPlumb.draggable("toto");
+  jsPlumb.addEndpoint("toto", {
   endpoint:"Dot",
   isSource:true,
   isTarget:true,
   anchor:[ "Left", { shape:"Square" } ]
-  });
 });
+  nodeAmount++;
+}
