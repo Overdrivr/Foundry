47057e0c04948cd954e42b76c3e22e5837fb0626
Rémi Bèges <remi.beges@gmail.com>
2015-11-19 12:01:24 +0100

    automatic anchor placement
    For #10

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -19,27 +19,54 @@ function createNode(){
   var totalsize = headersize + rowsize * (data.inputs + data.outputs)
   console.log(totalsize);
   var body = d3.select("div.cont");
+
+  // Create node
   var div = body.append("div")
               .attr("class","item")
               .attr("id","node" + nodeAmount)
               .style("height",totalsize + "px")
               .style("width", "80px");
-  var header = div.append("div")
-                .style("height",headersize)
-                .append("p")
-                .text("Perlin2D");
-
-              div.append("hr");
-
-  console.log(div);
-
+  // Make the node draggable
   jsPlumb.draggable(div.attr("id"));
-  jsPlumb.addEndpoint(div.attr("id"), {
-  endpoint:"Dot",
-  isSource:true,
-  isTarget:true,
-  anchor:[ 1, 0.8, 1, 0 ]
+  // Create header
+  var header = div.append("div")
+                .attr("class","nodetitle")
+                .style("height",headersize + "px")
+                //.append("p")
+                //.text("Perlin2D");
+
+
+  // Create each row
+  for(var i = 0 ; i < data.inputs ; i++){
+    // Create the row
+    var e = div.append("div")
+        .attr("class","noderow")
+        .style("height",rowsize + "px")
+        .style("width","100%")
+        //.append("p")
+        // /.text("input");
+
+    if(i == 0 || i == 2 || i == 4 || i == 6){
+      e.style("background","black")
+    }
+
+
+    var abspos = headersize + rowsize * i + rowsize/2;
+
+    var relpos = abspos / totalsize;
+    console.log(i,abspos)
+
+    jsPlumb.addEndpoint(div.attr("id"), {
+    endpoint:[ "Dot", { radius:5 } ],
+    isSource:true,
+    isTarget:true,
+    radius: 5,
+    anchor:[ 1, relpos, 1, 0 ]
+  });
+
+  }
+
   //anchor:[ "Right", { shape:"Square" } ]
-});
+
   nodeAmount++;
 }
